dccf7064e722190dc729a58c75098876a65f2453
Андрей <andre919andre@mail.ru>
2025-01-29 19:41:43 +0300

    Добовил блок услеги и адаптировал его

--- js/animation.js
+++ js/animation.js
@@ -1,19 +1,3 @@
-// gsap.from('.hero__text-box', {
-//   opacity: 0, duration: 2, x: -100,
-// })
-// gsap.from('.hero__title', {
-//   opacity: 0, duration: 2, x: -100, delay: .5
-// })
-// gsap.from('.hero__subtitle', {
-//   opacity: 0, duration: 2, x: -100, delay: 1
-// })
-// gsap.from('.hero__btn-link', {
-//   opacity: 0, duration: 2, x: -100, delay: 1.2, ease: "elastic.out(.5,0.3)"
-// })
-
-// gsap.from('.hero__img', {
-//   opacity: 0, duration: 3, z: 100, scale: 1.2
-// })
 
 let tl = gsap.timeline({ defaults: { duration: 1 } });
 let imgTl = gsap.timeline({ defaults: { duration: 2 } });
